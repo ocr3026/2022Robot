023ef42294afe9bbcfaaa6b4a13e4b8dbbcf5ebf
nullfedora <r100pogo@gmail.com>
2022-01-29 15:57:46 -0600

    gangstarap

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,11 +10,14 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Encoder;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -31,9 +34,12 @@ import ocr3026.util.Limelight.ledMode;
 import ocr3026.util.MecanumTankDrive;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -52,75 +58,68 @@ public class Robot extends TimedRobot {
 
   AHRS gyroscope = new AHRS();
 
-  CANSparkMax frontLeftMecanum = new CANSparkMax(29, MotorType.kBrushless);
-  CANSparkMax frontRightMecanum = new CANSparkMax(6, MotorType.kBrushless);
-  CANSparkMax backLeftMecanum = new CANSparkMax(22, MotorType.kBrushless);
-  CANSparkMax backRightMecanum = new CANSparkMax(18, MotorType.kBrushless);
+  CANSparkMax frontLeftTank = new CANSparkMax(29, MotorType.kBrushless);
+  CANSparkMax frontRightTank = new CANSparkMax(6, MotorType.kBrushless);
+  CANSparkMax backLeftTank = new CANSparkMax(22, MotorType.kBrushless);
+  CANSparkMax backRightTank = new CANSparkMax(18, MotorType.kBrushless);
   CANSparkMax leftTank = new CANSparkMax(50, MotorType.kBrushless);
   CANSparkMax rightTank = new CANSparkMax(51, MotorType.kBrushless);
-
+  MotorControllerGroup left = new MotorControllerGroup(frontLeftTank, leftTank, backLeftTank);
+  MotorControllerGroup right = new MotorControllerGroup(frontRightTank, rightTank, backRightTank);
+  DifferentialDrive drivetrain = new DifferentialDrive(left, right);
   Solenoid leftTankSolenoid = new Solenoid(1, PneumaticsModuleType.CTREPCM, 0);
   Solenoid rightTankSolenoid = new Solenoid(1, PneumaticsModuleType.CTREPCM, 1);
 
-  MecanumTankDrive drivetrain = new MecanumTankDrive(frontLeftMecanum, backLeftMecanum, leftTank, leftTankSolenoid, frontRightMecanum, backRightMecanum, rightTank, rightTankSolenoid);
-  
-  PIDController visionRotationController = new PIDController(1, 1, 1);
-  PIDController visionDistanceController = new PIDController(1, 1, 1);
-  PIDController gyroscoperotation = new PIDController(1, 1, 1);
-  boolean visionStage = false;
-  double visionSweetArea = 0.25;
-
-  CANSparkMax flywheel = new CANSparkMax(37, MotorType.kBrushless);
-
-  WPI_VictorSPX intake = new WPI_VictorSPX(53);
-  WPI_VictorSPX load = new  WPI_VictorSPX(21);
-  Solenoid kickup = new  Solenoid(PneumaticsModuleType.CTREPCM, 6);
-
-  DigitalInput ballloaded = new DigitalInput(1);
-  DigitalInput ballintake = new DigitalInput(2);
+  Encoder climberEncoder = new Encoder(0, 1);
 
   WPI_VictorSPX leftClimber = new WPI_VictorSPX(100);
   WPI_VictorSPX rightClimber = new WPI_VictorSPX(101);
   MotorControllerGroup climber = new MotorControllerGroup(leftClimber, rightClimber);
   DoubleSolenoid climberSolenoid = new DoubleSolenoid(1, PneumaticsModuleType.CTREPCM, 3, 4);
+
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    limelight = new Limelight();
 
     drivetrain.setDeadband(0.15d);
-    
-    frontRightMecanum.setInverted(true);
-    backRightMecanum.setInverted(true);
-    frontLeftMecanum.setIdleMode(IdleMode.kBrake);
-    backLeftMecanum.setIdleMode(IdleMode.kBrake);
-    frontRightMecanum.setIdleMode(IdleMode.kBrake);
-    backRightMecanum.setIdleMode(IdleMode.kBrake);
-  
+
     CameraServer.startAutomaticCapture();
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and
+   * test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-'   * SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
+   * ' * SmartDashboard integrated updating.
    */
- @Override
-  public void robotPeriodic() {}
+  @Override
+  public void robotPeriodic() {
+  }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
    * chooser code above as well.
    */
   @Override
@@ -133,152 +132,45 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
       case middleAuto:
-      if (gyroscope.getYaw() < 185){
-        drivetrain.MecanumRobotCentric(0,0, gyroscoperotation.calculate(gyroscope.getYaw()));
-      }
-      else if (gyroscope.getYaw() > 175){
-        drivetrain.MecanumRobotCentric(0,0, gyroscoperotation.calculate(gyroscope.getYaw()));
-      }
-      else {
-        if(frontLeftMecanum.getEncoder().getPosition() < 150){
-          drivetrain.MecanumRobotCentric(0, 0.75, 0);
-        }
-        else {
-          drivetrain.MecanumRobotCentric(0, 0, 0);
-        }
-      }
         break;
+
       case leftAuto:
-      if (gyroscope.getYaw() < 150){
-        drivetrain.MecanumRobotCentric(0,0, gyroscoperotation.calculate(gyroscope.getYaw()));
-      }
-      else if (gyroscope.getYaw() > 140){
-        drivetrain.MecanumRobotCentric(0,0, gyroscoperotation.calculate(gyroscope.getYaw()));
-      }
-      else {
-        if(frontLeftMecanum.getEncoder().getPosition() < 150){
-          drivetrain.MecanumRobotCentric(0, 0.75, 0);
-        }
-        else {
-          drivetrain.MecanumRobotCentric(0, 0, 0);
-        }
-      }
         break;
       case rightAuto:
-      if (gyroscope.getYaw() < 1230){
-        drivetrain.MecanumRobotCentric(0,0, gyroscoperotation.calculate(gyroscope.getYaw()));
-      } else if (gyroscope.getYaw() > 220) {
-        drivetrain.MecanumRobotCentric(0,0, gyroscoperotation.calculate(gyroscope.getYaw()));
-      }
-      else {
-        if(frontLeftMecanum.getEncoder().getPosition() < 150){
-          drivetrain.MecanumRobotCentric(0, 0.75, 0);
-        } else {
-          drivetrain.MecanumRobotCentric(0, 0, 0);
+          break;
         }
-        break;
-      }
     }
   }
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    if (joystick.getRawButtonPressed(12)) {
-      gyroscope.zeroYaw();
-    }
+  
 
-    if (joystick.getRawButtonPressed(2)) {
-      fieldtoggle.toggleValue();
-    }
+       // Phase 2: Get to sweet spot distance
 
-    if (xbox.getLeftTriggerAxis() > 0.9) {
-      // Vision
-      limelight.setCamMode(camMode.VISION);
-      limelight.setLedMode(ledMode.PIPELINE);
-
-      drivetrain.MecanumRobotCentric(0, 0, 0);
-
-      // Phase 1: Line up rotation
-
-      if (visionStage == false) {
-        if (-0.1 < limelight.getTargetX() && limelight.getTargetX() < 0.1) {
-          visionStage = true;
-        } else {
-          drivetrain.MecanumRobotCentric(0, 0, visionRotationController.calculate(limelight.getTargetX()));
-        }
-      }
-
-      // Phase 2: Get to sweet spot distance
-
-      if (visionStage == true) {
-        if (limelight.getTargetArea() == visionSweetArea) {
-          visionStage = false;
-        } else {
-          drivetrain.MecanumRobotCentric(0, visionDistanceController.calculate(limelight.getTargetArea(), visionSweetArea), 0);
-        }
-      }
-    } else {
-      // Driver
-      limelight.setCamMode(camMode.DRIVER);
-      limelight.setLedMode(ledMode.OFF);
-      
-      if (joystick.getRawButton(1)) {
-        drivetrain.TankDrive(joystick.getY(), steer.getX());
-      } else if (fieldtoggle.getValue()) {
-        drivetrain.MecanumFieldCentric(joystick.getY(), -joystick.getX(), steer.getX(), gyroscope.getYaw());
-      } else {
-        drivetrain.MecanumRobotCentric(joystick.getY(), -joystick.getX(), steer.getX());
-      }
-      
-    }
-
-    if (xbox.getRightTriggerAxis() > 0.9) {
-      flywheel.set(-1);
-      kickup.set(true);
-    } else {
-      flywheel.set(0);
-      kickup.set(false);
-    }
-
-    if (joystick.getRawButton(3) && ballintake.get() == false) {
-      if (ballloaded.get() == false) {
-        intake.set(1);
-        load.set(1);
-      } else {
-        intake.set(1);
-      }
-    } else {
-      intake.set(0);
-      load.set(0);
-    }
-
-    if (xbox.getYButton()) {
+    if (xbox.getYButton() && climberEncoder.getDistance() < 19) {
       climber.set(1);
-    } else if (xbox.getXButton()) {
+    } else if (xbox.getXButton() && climberEncoder.getDistance() > 2) {
       climber.set(-1);
     } else {
       climber.set(0);
     }
 
-    if(xbox.getLeftBumperPressed()) {
+    if (xbox.getLeftBumperPressed() && climberEncoder.getDistance() < 19) {
       climberSolenoid.set(Value.kForward);
-    } else if (xbox.getRightBumperPressed()) {
-    climberSolenoid.set(Value.kReverse);
+    } else if (xbox.getRightBumperPressed() && climberEncoder.getDistance() > 2) {
+      climberSolenoid.set(Value.kReverse);
     } else {
-    climberSolenoid.set(Value.kOff);
-    }
-
-    if(joystick.getRawButton(4) || xbox.getBButton()) {
-      intake.set(-1);
-    } else {
-      intake.set(0);
+      climberSolenoid.set(Value.kOff);
     }
   }
+
   /** This function is called once when the robot is disabled. */
   @Override
   public void disabledInit() {
@@ -287,13 +179,16 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
